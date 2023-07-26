2b7d8c5c229657b87632590450d9c227db7e0133
zibxto <christianaziba@gmail.com>
2023-07-26 12:51:10 +0100

    updated getline

--- getline.c
+++ getline.c
@@ -1,47 +1,46 @@
 #include "main.h"
 
 /**
- * _getline - stores into malloced buffer the user's command into shell
- * @str: buffer to store what was read to
- * Return: number of characters read or -1 on EOF
+ * _getline - Custom getline function for the shell
+ * @str: Double pointer to the buffer where the line will be stored
+ *
+ * Return: Number of characters read or -1 on error or EOF
  */
 ssize_t _getline(char **str)
 {
-	ssize_t i, no_of_char_read = 0;
-	char buff[1024];
-	char *newline_pos;
+	static char buffer[1024];
+	static ssize_t buff_size;
+	static ssize_t current_pos;
+	char c;
+	ssize_t num_read, line_len;
 
-	*str = NULL; /* Initialize the str pointer to NULL */
-	i = read(STDIN_FILENO, buff, 0);
-	/* Check for EOF before reading any characters */
-	if (i == 0)
-		return (-1);
-
-	while ((i = read(STDIN_FILENO, buff, 1023)) > 0)
+	if (buff_size == 0 || current_pos >= buff_size)
 	{
-		buff[i] = '\0'; /* Null-terminate the read data */
-
-		newline_pos = _strchr(buff, '\n');
-		if (newline_pos != NULL) /* Check if a newline character is found */
-		{
-			*newline_pos = '\0'; /* Null-terminate the line at the newline character */
-			/* Update the number of characters read to include the newline */
-			i = newline_pos - buff + 1;
-		}
-
-		no_of_char_read += i; /* Increment the total number of characters read */
-
-		/* Allocate/reallocate memory for str and concatenate buff to it */
-		*str = *str ? realloc(*str, no_of_char_read + 1)
-					: malloc(no_of_char_read + 1);
-		if (*str == NULL)
-			return (-1); /* Memory allocation failed */
-		_strcat(*str, buff);
-
-		if (newline_pos != NULL) /* If newline character is found, break the loop */
+		current_pos = 0;
+		buff_size = read(STDIN_FILENO, buffer, sizeof(buffer));
+		if (buff_size <= 0)
+			return (buff_size); /* Return -1 on error or EOF */
+	}
+	/* Allocate memory for the line buffer */
+	*str = malloc(buff_size + 1);
+	if (*str == NULL)
+		return (-1); /* Memory allocation failed */
+	line_len = 0; /* Number of characters in the current line */
+	while (current_pos < buff_size)
+	{
+		c = buffer[current_pos++];
+		(*str)[line_len++] = c;
+		/* If a newline is encountered, break the loop */
+		if (c == '\n')
 			break;
 	}
-
-	return (no_of_char_read);
+	/* Null-terminate the line */
+	(*str)[line_len] = '\0';
+	if (current_pos >= buff_size)
+	{
+		buff_size = 0;
+		current_pos = 0;
+	}
+	return (line_len);
 }
 
